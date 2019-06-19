df88bc37cccbf02a1ed9209931e97bb09d5430f7
niulinguo <niulinguo@163.com>
2019-06-19 11:51:09 +0800

    1.1 Release

--- photo_preview/src/main/java/com/lingo/photo_preview/PhotoPreviewActivity.java
+++ photo_preview/src/main/java/com/lingo/photo_preview/PhotoPreviewActivity.java
@@ -1,13 +1,20 @@
 package com.lingo.photo_preview;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import uk.co.senab.photoview.PhotoView;
 import uk.co.senab.photoview.PhotoViewAttacher;
@@ -36,11 +43,6 @@ public class PhotoPreviewActivity extends AppCompatActivity {
 
         mPhotoView = findViewById(R.id.photo_view);
 
-        Glide
-                .with(this)
-                .load(url)
-                .into(mPhotoView);
-
         mPhotoView.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
             @Override
             public void onViewTap(View view, float x, float y) {
@@ -49,5 +51,55 @@ public class PhotoPreviewActivity extends AppCompatActivity {
 
             }
         });
+
+        findViewById(R.id.cl_container).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityCompat.finishAfterTransition(PhotoPreviewActivity.this);
+
+            }
+        });
+
+        loadUrl();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (intent.hasExtra("url")) {
+            url = intent.getStringExtra("url");
+        }
+
+        if (TextUtils.isEmpty(url)) {
+            finish();
+            return;
+        }
+
+        loadUrl();
+    }
+
+    private void loadUrl() {
+
+        Glide
+                .with(this)
+                .asBitmap()
+                .load(url)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        mPhotoView.setImageBitmap(resource);
+                    }
+
+                    @Override
+                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                        super.onLoadFailed(errorDrawable);
+
+                        Toast.makeText(PhotoPreviewActivity.this, "图片已损坏", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
     }
 }
